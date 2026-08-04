3855668afe90ce58887c16db57540d7eeacf8b2b
cybermaksx <cybermaksx@proton.me>
2026-08-04 16:43:22 +0400

    Two steps back . Now i wrote the script so i can understand what scapy is seeing so i can filter data better

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from scapy.all import rdpcap
 
-print("Starting analysis...")
-
+print(f"Reading the pcap file...")
 
 packets = rdpcap("test.pcapng")
 
 
-print(f"✅ Succesfully read : {len(packets)}")
+for packet in packets:
+    packet.show()
 
 
-print("\nFirst 3 packets:")
-for i in range(3):
-    print(f"  {i+1}. {packets[i].summary()}")
+
+
+
+
+
+
+
