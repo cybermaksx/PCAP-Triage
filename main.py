2c28cb90869c54d6d856b2469a7ed384d3b1bcf1
cybermaksx <cybermaksx@proton.me>
2026-08-05 14:38:59 +0400

    Skeleton completed. Time to write functions one by one

--- main.py
+++ main.py
@@ -7,9 +7,47 @@ packets = rdpcap("test.pcapng")
 
 for packet in packets:
     packet.show()
+    input()
 
 
 
+def stat():
+    #How many packets
+    #How many TCP packets
+    #How many UDP packets
+    #and etc
+
+
+
+
+def threat_checking ():
+    #Checking pcap file on man in the middle attacks and etc
+
+
+
+
+
+def report_generator():
+    #coming soon
+
+
+
+def main():
+    pcap_file = "test.pcapng"
+
+    packets = rdpcap(pcap_file)
+
+    get_statistics(packets)
+
+    threat_checking(packets)
+
+    report_generator([])
+
+
+
+
+if __name__ == "__main__":
+    main()
 
 
 
